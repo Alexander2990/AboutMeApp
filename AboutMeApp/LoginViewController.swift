760a8cf962c9fbe9fda3a92eae7fd4bc628ce5d2
Alexander <Alexander29_90@icloud.com>
2024-02-11 22:09:44 +0500

    Implemented application logic

--- AboutMeApp/LoginViewController.swift
+++ AboutMeApp/LoginViewController.swift
@@ -7,26 +7,62 @@
 
 import UIKit
 
-final class LoginViewController: UIViewController {
+struct User {
+    let user: String
+    let password: String
+}
 
+final class LoginViewController: UIViewController {
+    
+    // MARK: - IB Outlets
     @IBOutlet var userNameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-
-    @IBAction func showAuthorizationData(_ sender: UIButton) {
-        
-        sender.tag == 0 ? 
-        showAlert(withTitle: "Oops!", andMessage: "Your name is 1 😉") : 
-        showAlert(withTitle: "Oops!", andMessage: "Your password is 1 😉")
+    // MARK: - Private Properties
+    private let userData = User(user: "Alexander", password: "1")
+    
+    // MARK: - Overrides Methods
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let welcomeVC = segue.destination as? WelcomeViewController
+        welcomeVC?.userName = userNameTextField.text
     }
     
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        guard userNameTextField.text == userData.user, passwordTextField.text == userData.password else {
+            showAlert(
+                withTitle: "Invalid login or password",
+                andMessage: "Please enter correct login and password"
+            )
+            return false
+        }
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    // MARK: - IB Actions
+    @IBAction func showAuthorizationData(_ sender: UIButton) {
+        sender.tag == 0 ? showAlert(
+            withTitle: "Oops!",
+            andMessage: "Your User Name is Alexander 😉"
+        ) : showAlert(withTitle: "Oops!", andMessage: "Your password is 1 😉")
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+    // MARK: - Private Methods
     private func showAlert(withTitle title: String, andMessage message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.userNameTextField.text = ""
+            self.passwordTextField.text = ""
+        }
         alert.addAction(okAction)
         present(alert, animated: true)
     }
--- AboutMeApp/WelcomeViewController.swift
+++ AboutMeApp/WelcomeViewController.swift
@@ -7,23 +7,15 @@
 
 import UIKit
 
-class WelcomeViewController: UIViewController {
+final class WelcomeViewController: UIViewController {
 
+    @IBOutlet var userGreetingLabel: UILabel!
+    
+    var userName: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        userGreetingLabel.text = "Welcome \(userName ?? "")"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
