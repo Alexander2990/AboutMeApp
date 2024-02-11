6f7da34c8f3aa8f3ad31641a44c72af8f6e35358
Alexander <Alexander29_90@icloud.com>
2024-02-11 19:21:07 +0500

    Added func showAuthorizationData

--- AboutMeApp/LoginViewController.swift
+++ AboutMeApp/LoginViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 final class LoginViewController: UIViewController {
 
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-
+    @IBAction func showAuthorizationData(_ sender: UIButton) {
+        
+        sender.tag == 0 ? 
+        showAlert(withTitle: "Oops!", andMessage: "Your name is 1 😉") : 
+        showAlert(withTitle: "Oops!", andMessage: "Your password is 1 😉")
+    }
+    
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
 }
 
