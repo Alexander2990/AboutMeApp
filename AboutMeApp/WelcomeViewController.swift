ff1e9a86bcc5562f08f46490d3782fa4373348e2
Alexander <Alexander29_90@icloud.com>
2024-07-26 17:43:18 +0500

    Setup UI

---
+++ AboutMeApp/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeViewController.swift
+//  AboutMeApp
+//
+//  Created by Александр on 26.07.2024.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
