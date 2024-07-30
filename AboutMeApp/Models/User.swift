52de1988418a7c59d00e435535c41a711862d3a3
Alexander <Alexander29_90@icloud.com>
2024-07-30 10:12:21 +0500

    Created by User model

---
+++ AboutMeApp/Models/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  AboutMeApp
+//
+//  Created by Александр on 30.07.2024.
+//
+
+struct User {
+    
+    let login: String
+    let password: String
+    let person: Person
+}
+
+struct Person {
+    
+    let name: String
+    let surname: String
+    let company: String
+    let department: String
+    let jobTitle: String
+}
